802ab5e2c950894716140652e60e316101569939
Mausam Singh <53150785+Kingbond470@users.noreply.github.com>
2021-06-29 00:06:50 +0530

    Layout You

---
+++ app/src/main/java/com/example/layoutyou/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.layoutyou;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.masai_team_layout);
+    }
+}
